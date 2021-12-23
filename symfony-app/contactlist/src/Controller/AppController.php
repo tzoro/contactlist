1da60f5b97366d3ef3c8672a1779bdf8e41da8ee
Tino <tino@klassapp.com>
2021-12-23 09:39:43 +0200

    - Routing added, ORM data test - Kint added

--- symfony-app/contactlist/src/Controller/AppController.php
+++ symfony-app/contactlist/src/Controller/AppController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
 
 class AppController extends AbstractController
 {
@@ -13,9 +14,32 @@ class AppController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AppController.php',
+
+
+
+      $entityManager = $this->getDoctrine()->getManager();
+      $repo = new Contact();
+      $repo->setFirstName('tzoro');
+      
+      $entityManager->persist($repo);
+      $entityManager->flush();
+
+
+      $repository = $this->getDoctrine()->getRepository(Contact::class);
+      $cached = $repository->findAll();
+      // d($cached);exit;
+
+        return $this->render('test/index.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'users' => $cached,
+            'notifications' => 2,
         ]);
+
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/AppController.php',
+        // ]);
     }
 }
